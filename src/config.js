8efaf89cb26afc3f14b1f7e2f57f52c8106ea3f6
Mike Dent <mikedent85@gmail.com>
2021-02-02 10:08:39 -0500

    deploying with heroku endpoint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const config = {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://secure-brook-57938.herokuapp.com/api',
   TOKEN_KEY: 'change-this-secret',
 }
 
